bf9a63bb0fb80e7b46af5d19a31f89663f0991c3
eunhee <6810779s@naver.com>
2024-03-28 19:58:28 +0900

    [feat] 필요한 라이브러리 설치, react-router-dom, vite프로젝트로 변환, axios, react-query,react-hook-form 설치, 기본 페이지 router로 설정

---
+++ front/src/components/SnbLayout.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+
+const SnbLayout = () => {
+  return (
+    <div>
+      Snb
+      <Outlet />
+    </div>
+  );
+};
+
+export default SnbLayout;
---
+++ front/vite.config.ts
@@ -0,0 +1,13 @@
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig, loadEnv } from "vite";
+import svgr from "vite-plugin-svgr";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig(({ mode }) => {
+  loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react(), tsconfigPaths(), svgr()],
+    base: "./",
+  };
+});
