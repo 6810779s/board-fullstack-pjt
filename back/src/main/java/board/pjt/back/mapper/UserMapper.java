6dacb22365e38193e9f20b52f3accca7b5b404f4
eunhee <6810779s@naver.com>
2024-06-16 18:29:57 +0900

    [feat] user test code 작성 완료

---
+++ back/src/main/java/board/pjt/back/mapper/UserMapper.java
@@ -0,0 +1,22 @@
+package board.pjt.back.mapper;
+
+import board.pjt.back.domain.UserDto;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Mapper
+@Repository
+public interface UserMapper {
+    UserDto select(String email);
+
+    List<UserDto> selectAll();
+
+    int insert(UserDto dto);
+
+    int deleteUser(String email);
+
+    int updateUser(UserDto dto);
+
+}
