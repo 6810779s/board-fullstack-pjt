713a138567a88cdac215adbde0a47153e44ba5ed
eunhee <6810779s@naver.com>
2024-08-07 19:26:21 +0900

    [feat] like 개수 추가

--- back/src/main/java/board/pjt/back/controller/BoardLikeController.java
+++ back/src/main/java/board/pjt/back/controller/BoardLikeController.java
@@ -4,12 +4,10 @@ import board.pjt.back.common.codes.SuccessCode;
 import board.pjt.back.common.response.ApiResponse;
 import board.pjt.back.dao.BoardLikeDao;
 import board.pjt.back.domain.boardLike.BoardLikeListRequestDto;
-import board.pjt.back.domain.boardLike.BoardLikeResponseDto;
+import board.pjt.back.domain.boardLike.BoardLikeListResponseDto;
 import board.pjt.back.domain.boardLike.BoardToggleLikeRequestDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/board-like")
@@ -21,9 +19,9 @@ public class BoardLikeController {
     }
 
     @GetMapping("/")
-    public ResponseEntity<ApiResponse<List<BoardLikeResponseDto>>> getBoardLikeList(@RequestBody BoardLikeListRequestDto requestDto) {
-        List<BoardLikeResponseDto> boardLikeList = boardLikeDao.findByBoardLikeId(requestDto);
-        ApiResponse<List<BoardLikeResponseDto>> response = ApiResponse.of(SuccessCode.SELECT_SUCCESS, boardLikeList);
+    public ResponseEntity<ApiResponse<BoardLikeListResponseDto>> getBoardLikeList(@RequestBody BoardLikeListRequestDto requestDto) {
+        BoardLikeListResponseDto boardLikeList = boardLikeDao.findByBoardLikeId(requestDto);
+        ApiResponse<BoardLikeListResponseDto> response = ApiResponse.of(SuccessCode.SELECT_SUCCESS, boardLikeList);
         return ResponseEntity.ok(response);
     }
 
