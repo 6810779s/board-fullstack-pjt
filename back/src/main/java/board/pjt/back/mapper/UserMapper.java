c57f701ce3f13489ac59904d13f7384359bb341e
eunhee <6810779s@naver.com>
2024-08-20 23:40:14 +0900

    [feat] 1. 회원가입 구현 2. 로그인 jwt 구현

--- back/src/main/java/board/pjt/back/mapper/UserMapper.java
+++ back/src/main/java/board/pjt/back/mapper/UserMapper.java
@@ -1,17 +1,19 @@
 package board.pjt.back.mapper;
 
-import board.pjt.back.domain.UserDto;
+import board.pjt.back.dto.UserDto;
+import board.pjt.back.dto.user.UserJoinRequestDto;
+import board.pjt.back.dto.user.UserResponseDto;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
 @Mapper
 public interface UserMapper {
-    UserDto select(String email);
+    UserResponseDto select(String email);
 
     List<UserDto> selectAll();
 
-    int insert(UserDto dto);
+    void insert(UserJoinRequestDto requestDto);
 
     int deleteUser(String email);
 
