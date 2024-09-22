9c6dc9fcab800294ca742b32a254d4c567184d5e
eunhee <6810779s@naver.com>
2024-09-22 19:17:53 +0900

    [feat] 공통 컴포넌트 적용. Alert, Button속성 추가, Tabs

---
+++ front/src/stories/Dialog.stories.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import { Button, Dialog, Stack } from '@mui/material';
+import { Meta, StoryFn } from '@storybook/react';
+
+import Alert, { AlertProps } from '@/components/Alert';
+
+const meta = {
+    title: 'Example/Dialog',
+    component: Dialog,
+    parameters: { backgrounds: { default: 'dark' }, layout: 'centered' },
+    tags: ['autodocs'],
+    argTypes: {},
+    args: {},
+} satisfies Meta<typeof Dialog>;
+
+export default meta;
+
+type Story = StoryFn<typeof Alert>;
+const Template = (props: AlertProps) => {
+    const [open, setOpen] = React.useState<boolean>(false);
+
+    return (
+        <Stack>
+            <Button onClick={() => setOpen(true)}>클릭</Button>
+            <Alert
+                {...props}
+                open={open}
+                onClose={() => setOpen(false)}
+                onConfirm={() => {
+                    console.log('확인을 눌렀어요!');
+                    setOpen(false);
+                }}
+            />
+        </Stack>
+    );
+};
+
+export const ConfirmDialog: Story = Template.bind({});
+export const ErrorDialog: Story = Template.bind({});
+
+ConfirmDialog.args = {
+    variant: 'confirm',
+    title: 'title',
+    content: 'content',
+};
+ErrorDialog.args = {
+    variant: 'error',
+    title: 'title',
+    content: 'content',
+};
