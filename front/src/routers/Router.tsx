dc8e0ff671cbea1da3cd27d5eba2d7dc1fca581e
eunhee <6810779s@naver.com>
2024-09-24 21:33:24 +0900

    [bugfix] 홈화면 path /home 에서 /으로 일괄 변경

--- front/src/routers/Router.tsx
+++ front/src/routers/Router.tsx
@@ -25,7 +25,7 @@ export const router = createBrowserRouter([
             //회원가입
             { path: 'sign-up', element: <SignUp /> },
             //홈
-            { path: 'home', element: <Home /> },
+            { index: true, element: <Home /> },
             //팀원 구하기 게시판 목록
             {
                 path: 'board-list',
