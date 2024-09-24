f663ba6c0c41656d956f9be3a495709120474ef5
eunhee <6810779s@naver.com>
2024-09-24 21:26:51 +0900

    [feat] 로그인 회원가입 화면 구현

--- front/src/pages/auth/signUp/index.tsx
+++ front/src/pages/auth/signUp/index.tsx
@@ -1,3 +1,64 @@
+import { useNavigate } from 'react-router-dom';
+
+import { Button, IconButton, Stack, TextField, Typography } from '@mui/material';
+import { CalendarBlank } from '@phosphor-icons/react';
+
 export const SignUp = () => {
-    return <div>회원가입</div>;
+    const navigate = useNavigate();
+    return (
+        <Stack flex={1} alignItems="center" justifyContent="center">
+            <Stack gap="30px" sx={{ width: '523px' }}>
+                <Typography
+                    sx={{ textAlign: 'center', fontSize: '40px', fontWeight: 700, flex: 1 }}
+                >
+                    회원가입
+                </Typography>
+                <Stack gap="16px">
+                    <Stack gap="4px">
+                        <Typography>이메일</Typography>
+                        <TextField placeholder="이메일을 입력해주세요." />
+                    </Stack>
+                    <Stack gap="4px">
+                        <Typography>비밀번호</Typography>
+                        <TextField placeholder="비밀번호를 입력해주세요." />
+                    </Stack>
+                    <Stack gap="4px">
+                        <Typography>이름</Typography>
+                        <TextField placeholder="실제 이름을 입력해주세요." />
+                    </Stack>
+                    <Stack gap="4px">
+                        <Typography>생년월일</Typography>
+                        <TextField
+                            placeholder="1997-07-02"
+                            slotProps={{
+                                input: {
+                                    endAdornment: (
+                                        <IconButton onClick={() => {}}>
+                                            <CalendarBlank size="16px" />
+                                        </IconButton>
+                                    ),
+                                },
+                            }}
+                        />
+                    </Stack>
+                    <Stack gap="4px">
+                        <Typography>이메일</Typography>
+                        <TextField placeholder="이메일을 입력해주세요." />
+                    </Stack>
+                    <Stack direction="row" gap="18px">
+                        <Button
+                            variant="WhiteContainedBlackOutlined"
+                            sx={{ flex: 1 }}
+                            onClick={() => navigate('/login')}
+                        >
+                            이미 가입하셨나요? 로그인하기
+                        </Button>
+                        <Button sx={{ flex: 1 }} onClick={() => navigate('/sign-up')}>
+                            가입하기
+                        </Button>
+                    </Stack>
+                </Stack>
+            </Stack>
+        </Stack>
+    );
 };
