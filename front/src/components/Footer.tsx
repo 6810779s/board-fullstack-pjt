d54aad2ca4ef93eb99ecb2e0e705b5d01e4bd0b5
eunhee <6810779s@naver.com>
2024-09-24 20:18:06 +0900

    [feat] footer 페이지레이아웃에 적용

---
+++ front/src/components/Footer.tsx
@@ -0,0 +1,17 @@
+import { Stack, Typography } from '@mui/material';
+
+import { palette } from '@/themes';
+
+export const Footer = () => {
+    return (
+        <Stack
+            justifyContent="center"
+            alignItems="center"
+            sx={{ height: '220px', background: palette.common.white }}
+        >
+            <Typography sx={{ fontSize: '20px' }}>
+                © 2024 Eunhee’s PJT reviewer. All Rights Reserved.
+            </Typography>
+        </Stack>
+    );
+};
