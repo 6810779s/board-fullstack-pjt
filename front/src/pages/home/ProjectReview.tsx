de15ed75e8cab985f4c5128ecc300d694578d30c
eunhee <6810779s@naver.com>
2024-09-25 20:20:00 +0900

    [feat] login 전일 경우 보여지는 홈 화면

--- front/src/pages/home/ProjectReview.tsx
+++ front/src/pages/home/ProjectReview.tsx
@@ -6,6 +6,7 @@ import { ProjectReviewCard } from '@/components/ProjectReviewCard';
 
 export const ProjectReview = () => {
     const navigate = useNavigate();
+    const login = false;
     return (
         <Stack direction="row" sx={{ padding: '60px 0' }}>
             <Stack justifyContent="center" gap="24px" flex={1}>
@@ -15,27 +16,42 @@ export const ProjectReview = () => {
                     프로젝트 리뷰 더보기
                 </Button>
             </Stack>
-            <Stack direction="row" gap="40px" flex={1}>
-                <ProjectReviewCard
-                    imgSrc={'/src/assets/book_img.svg'}
-                    title="자바 실시간 채팅 프로그램"
-                    starCnt={3}
-                    member={3}
-                    startDate="2024.05.02"
-                    endDate="2024.06.07"
-                    commentCnt={5}
-                    hashTags={['JAVA', 'Spring', 'React']}
-                />
-                <ProjectReviewCard
-                    imgSrc={'/src/assets/code_img.svg'}
-                    title="자바 실시간 채팅 프로그램"
-                    starCnt={3}
-                    member={3}
-                    startDate="2024.05.02"
-                    endDate="2024.06.07"
-                    commentCnt={5}
-                    hashTags={['JAVA', 'Spring', 'React']}
-                />
+
+            <Stack flex={1} sx={{ position: 'relative' }}>
+                <Button
+                    sx={{
+                        position: 'absolute',
+                        transform: 'translate(-50%,-50%)',
+                        top: '50%',
+                        left: '50%',
+                        zIndex: 999,
+                    }}
+                    variant="WhiteContainedBlackOutlined"
+                >
+                    로그인 하러 가기
+                </Button>
+                <Stack direction="row" gap="40px" sx={{ filter: login ? 'none' : 'blur(10px)' }}>
+                    <ProjectReviewCard
+                        imgSrc={'/src/assets/book_img.svg'}
+                        title="자바 실시간 채팅 프로그램"
+                        starCnt={3}
+                        member={3}
+                        startDate="2024.05.02"
+                        endDate="2024.06.07"
+                        commentCnt={5}
+                        hashTags={['JAVA', 'Spring', 'React']}
+                    />
+                    <ProjectReviewCard
+                        imgSrc={'/src/assets/code_img.svg'}
+                        title="자바 실시간 채팅 프로그램"
+                        starCnt={3}
+                        member={3}
+                        startDate="2024.05.02"
+                        endDate="2024.06.07"
+                        commentCnt={5}
+                        hashTags={['JAVA', 'Spring', 'React']}
+                    />
+                </Stack>
             </Stack>
         </Stack>
     );
