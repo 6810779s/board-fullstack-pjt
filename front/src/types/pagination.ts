18890e522f64ae24c315a6144f5c4b7b50e3a17a
eunhee <6810779s@naver.com>
2024-12-17 21:59:04 +0900

    [feat] pagination 타입 추가

--- front/src/types/pagination.ts
+++ front/src/types/pagination.ts
@@ -1,12 +1,12 @@
 export interface TPagination<T> {
-    contents:T[];
-    totalItems:number;
-    totalPages:number;
-    pageSize:number;
-    page:number;
-    numberOfElements:number;
-    hasPrevious:boolean;
-    hasNext:boolean;
-    first:boolean;
-    last:boolean;
+    contents: T[];
+    totalItems: number;
+    totalPages: number;
+    pageSize: number;
+    page: number;
+    numberOfElements: number;
+    hasPrevious: boolean;
+    hasNext: boolean;
+    first: boolean;
+    last: boolean;
 }
